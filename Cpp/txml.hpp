1930969bfe7fc40ca730884bf8f6dc585534b05f
Technolandia <renemuala@icloud.com>
2020-07-18 23:41:48 -0700

    classe criada

--- Cpp/txml.hpp
+++ Cpp/txml.hpp
@@ -3,4 +3,20 @@ define txml_hpp
 
 #include <iostream>
 
+/* em C++ classes sao o ponto pe partida para oo */
+class TXML{
+public: 
+
+  // inicializacao da minha class 
+  TXML();
+
+  // liberacao de memoria
+  ~TXML();
+ 
+private:
+
+  // dados do arquivo txml
+  std::string Data;
+};
+
 endif
